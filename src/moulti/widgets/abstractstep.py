0629dee4f6a63e0b3296189b112acc5cf52b1181
Xavier G. <xavier.moulti@kindwolf.org>
2024-02-13 20:40:47 +0100

    Introduce the AbstractStep class.
    The point of this class is to develop other step-like widgets.

---
+++ src/moulti/widgets/abstractstep.py
@@ -0,0 +1,100 @@
+from textual.app import ComposeResult
+from textual.widgets import Static, Collapsible
+
+class AbstractStep(Static):
+	"""
+	This is the base class for all components end users may wish to add to Moulti.
+	"""
+	def __init__(self, id: str, **kwargs: str|int|bool): # pylint: disable=redefined-builtin
+		self.collapsible = Collapsible(title=id)
+		self.top_label = Static('', classes='top_text')
+		self.bottom_label = Static('', classes='bottom_text')
+
+		self.top_text = ''
+		self.bottom_text = ''
+
+		self.init_kwargs = kwargs
+
+		step_classes = str(kwargs.get('classes', ''))
+		super().__init__(id=id, classes=step_classes)
+
+	def subcompose(self) -> ComposeResult:
+		return []
+
+	def compose(self) -> ComposeResult:
+		with self.collapsible:
+			yield self.top_label
+			yield from self.subcompose()
+			yield self.bottom_label
+
+	def on_mount(self) -> None:
+		self.update_properties(self.init_kwargs)
+
+	def title_from_id(self) -> str:
+		self_id = str(self.id)
+		if '_' not in self_id:
+			return self_id
+		return self_id.split('_', 1)[1]
+
+	def update_properties(self, kwargs: dict[str, str|int|bool]) -> None:
+		if 'classes' in kwargs:
+			self.set_classes(str(kwargs['classes']))
+		if 'title' in kwargs:
+			self.collapsible.title = str(kwargs['title']) if kwargs['title'] else self.title_from_id()
+		if 'collapsed' in kwargs:
+			self.collapsible.collapsed = bool(kwargs['collapsed'])
+		if 'top_text' in kwargs:
+			self.top_text = str(kwargs['top_text'])
+			self.top_label.update(self.top_text)
+		self.top_label.styles.display = 'block' if self.top_text else 'none'
+		if 'bottom_text' in kwargs:
+			self.bottom_text = str(kwargs['bottom_text'])
+			self.bottom_label.update(self.bottom_text)
+		self.bottom_label.styles.display = 'block' if self.bottom_text else 'none'
+
+	DEFAULT_COLORS = """
+	$step_default: $primary;
+	$step_success: ansi_bright_green;
+	$step_warning: orange;
+	$step_error: tomato;
+	$step_debug: $accent;
+	"""
+	DEFAULT_CSS = DEFAULT_COLORS + """
+	AbstractStep {
+		/* If we do not specify "auto" here, each step will take 100% of the viewport: */
+		height: auto;
+		background: $step_default;
+		color: auto;
+		&.success { background: $step_success; }
+		&.warning { background: $step_warning; }
+		&.error { background: $step_error; }
+		&.debug { background: $step_debug; }
+		/* Compact design: no padding, no margins, no borders: */
+		& Collapsible {
+			padding: 0;
+			margin: 0;
+			border: none;
+		}
+		& CollapsibleTitle {
+			padding: 0;
+			width: 100%;
+		}
+		& CollapsibleTitle:hover {
+			background: $foreground 80%;
+		}
+		& CollapsibleTitle:focus {
+			background: initial;
+		}
+		/* Collapsible contents: */
+		& Contents {
+			/* Leave some space on each side of the contents: */
+			padding-left: 2 !important;
+			padding-right: 1 !important;
+		}
+	}
+	AbstractStep:focus, AbstractStep:focus-within {
+		& CollapsibleTitle {
+			text-style: bold;
+		}
+	}
+	"""
--- src/moulti/widgets/step.py
+++ src/moulti/widgets/step.py
@@ -1,61 +1,35 @@
 from textual.app import ComposeResult
-from textual.widgets import Static, Collapsible
 from rich.text import Text
+from .abstractstep import AbstractStep
 from .moultilog import MoultiLog
 
 ANSI_ESCAPE_SEQUENCE = '\x1b'
 
-class Step(Static):
+class Step(AbstractStep):
 	"""
 	This widget represents a step in a script, program or process.
 	Visually speaking, it is essentially a collapsible text area surrounded
 	with optional text lines.
 	"""
 	def __init__(self, id: str, **kwargs: str|int|bool): # pylint: disable=redefined-builtin
-		self.collapsible = Collapsible(title=id)
-		self.top_label = Static('', classes='top_text')
 		self.log_widget = MoultiLog(highlight=False)
-		self.bottom_label = Static('', classes='bottom_text')
 
-		self.top_text = ''
 		self.min_height = 1
 		self.max_height = 25
-		self.bottom_text = ''
 
-		self.init_kwargs = kwargs
 		# This attribute is meant to prevent deletion of a step while content
 		# is being appended to it:
 		self.prevent_deletion = 0
 
-		step_classes = str(kwargs.get('classes', ''))
-		super().__init__(id='step_' + id, classes=step_classes)
+		super().__init__(id='step_' + id, **kwargs)
 
 		self.color = ''
 
-	def compose(self) -> ComposeResult:
-		with self.collapsible:
-			yield self.top_label
-			yield self.log_widget
-			yield self.bottom_label
-
-	def on_mount(self) -> None:
-		self.update_properties(self.init_kwargs)
+	def subcompose(self) -> ComposeResult:
+		yield self.log_widget
 
 	def update_properties(self, kwargs: dict[str, str|int|bool]) -> None:
-		if 'classes' in kwargs:
-			self.set_classes(str(kwargs['classes']))
-		if 'title' in kwargs:
-			self.collapsible.title = str(kwargs['title']) if kwargs['title'] else str(self.id)[5:]
-		if 'collapsed' in kwargs:
-			self.collapsible.collapsed = bool(kwargs['collapsed'])
-		if 'top_text' in kwargs:
-			self.top_text = str(kwargs['top_text'])
-			self.top_label.update(self.top_text)
-		self.top_label.styles.display = 'block' if self.top_text else 'none'
-		if 'bottom_text' in kwargs:
-			self.bottom_text = str(kwargs['bottom_text'])
-			self.bottom_label.update(self.bottom_text)
-		self.bottom_label.styles.display = 'block' if self.bottom_text else 'none'
+		super().update_properties(kwargs)
 		if 'text' in kwargs:
 			self.clear()
 			self.append(str(kwargs['text']))
@@ -99,63 +73,22 @@ class Step(Static):
 			return style.render('_').split('_')[0]
 		return ''
 
-	DEFAULT_CSS = """
-	$step_default: $primary;
-	$step_success: ansi_bright_green;
-	$step_warning: orange;
-	$step_error: tomato;
-	$step_debug: $accent;
-
+	DEFAULT_CSS = AbstractStep.DEFAULT_COLORS + """
 	Step {
-		/* If we do not specify "auto" here, each step will take 100% of the viewport: */
-		height: auto;
-		background: $step_default;
-		color: auto;
 		& MoultiLog {
 			scrollbar-corner-color: $step_default;
 		}
 		&.success {
-			background: $step_success;
 			& MoultiLog { scrollbar-corner-color: $step_success; }
 		}
 		&.warning {
-			background: $step_warning;
 			& MoultiLog { scrollbar-corner-color: $step_warning; }
 		}
 		&.error {
-			background: $step_error;
 			& MoultiLog { scrollbar-corner-color: $step_error; }
 		}
 		&.debug {
-			background: $step_debug;
 			& MoultiLog { scrollbar-corner-color: $step_debug; }
-		}
-		/* Compact design: no padding, no margins, no borders: */
-		& Collapsible {
-			padding: 0;
-			margin: 0;
-			border: none;
-		}
-		& CollapsibleTitle {
-			padding: 0;
-			width: 100%;
-		}
-		& CollapsibleTitle:hover {
-			background: $foreground 80%;
-		}
-		& CollapsibleTitle:focus {
-			background: initial;
-		}
-		/* Collapsible contents: */
-		& Contents {
-			/* Leave some space on each side of the contents: */
-			padding-left: 2 !important;
-			padding-right: 1 !important;
-		}
-	}
-	Step:focus, Step:focus-within {
-		& CollapsibleTitle {
-			text-style: bold;
 		}
 	}
 	"""

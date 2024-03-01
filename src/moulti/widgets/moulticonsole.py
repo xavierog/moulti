9647a664e84eb3af699cb52c33414def2b8c8147
Xavier G. <xavier.moulti@kindwolf.org>
2024-03-02 00:20:27 +0100

    MoultiConsole: add highlighting.

--- src/moulti/widgets/moulticonsole.py
+++ src/moulti/widgets/moulticonsole.py
@@ -1,10 +1,23 @@
 from typing import Any
 from typing_extensions import Self
 from pyperclip import copy # type: ignore
+from rich.highlighter import Highlighter, RegexHighlighter, ReprHighlighter
+from rich.text import Text
+from rich.theme import Theme
 from textual.app import ComposeResult
 from textual.events import Click
 from textual.widgets import Label, Static
 from .moultilog import MoultiLog
+
+MOULTI_THEME = Theme({
+    'moulti.date': 'orchid2',
+    'moulti.time': 'orchid2',
+    'moulti.socket': 'steel_blue1',
+    'moulti.fileno': 'orchid1',
+    'moulti.function': 'slate_blue1',
+    'moulti.messagein': 'bold dodger_blue2',
+    'moulti.messageout': 'bold chartreuse3',
+})
 
 class MoultiConsole(Static):
 	BINDINGS = [
@@ -15,8 +28,11 @@ class MoultiConsole(Static):
 	def __init__(self, *args: Any, **kwargs: Any) -> None:
 		super().__init__(*args, **kwargs)
 		self.log_widget = MoultiLog()
+		self.log_widget.highlighter = MoultiConsoleHighlighter()
+		self.log_widget.highlight = True
 
 	def compose(self) -> ComposeResult:
+		self.app.console.push_theme(MOULTI_THEME)
 		yield Label('Moulti console')
 		yield self.log_widget
 
@@ -59,3 +75,65 @@ class MoultiConsole(Static):
 		}
 	}
 	"""
+
+class MoultiRegexHighlighter(RegexHighlighter):
+	RE_EXT_MODE = '(?x) # extended mode'
+	DATE = r'''
+(?P<date>
+	[1-9][0-9]*-           # year
+	(?:0[1-9]|1[0-2])-     # month
+	(?:[012][0-9]|3[01])   # day
+)'''
+	TIME = r'''
+(?P<time>
+	(?:[01][0-9]|2[0-3]):  # hours
+	(?:[0-5][0-9]):        # minutes
+	(?:[0-5][0-9])\.       # seconds
+	[0-9]+                 # milliseconds
+)'''
+	DATETIME = DATE + '[T ]' + TIME
+	SOCKET = r'(?P<socket>[@/].*\.socket)'
+	FILENO = r'fd(?P<fileno>\d+)'
+	SOCKET_FILENO = SOCKET + ':' + FILENO
+	FUNCTION = r'(?P<function>accept|exec|network loop|pass|read|reply|save)'
+	MESSAGE_IN = r'(?P<messagein>=>)'
+	MESSAGE_OUT = r'(?P<messageout><=)'
+	MESSAGE = '(?:' + MESSAGE_IN + '|' + MESSAGE_OUT + ')'
+
+	base_style: str = "moulti."
+	highlights = [
+		RE_EXT_MODE + '^' + DATETIME + ' ',
+		' ' + SOCKET_FILENO + ': ',
+		SOCKET,
+		' ' + FUNCTION + ': ',
+		': ' + MESSAGE + ' ',
+	]
+
+class MoultiConsoleHighlighter(Highlighter):
+	SEPARATORS = (' => ', ' <= ', ' exec: ')
+	"""
+	Everything to the right of these separators is to be highlighted by ReprHighlighter.
+	Everything to the left of these separators is to be highlighted by MoultiRegexHighlighter.
+	"""
+
+	def __init__(self) -> None:
+		self.repr_hl = ReprHighlighter()
+		self.moulti_re_hl = MoultiRegexHighlighter()
+		super().__init__()
+
+	def highlight(self, text: Text) -> None:
+		plain_text = text.plain
+		for separator in self.SEPARATORS:
+			if separator in plain_text:
+				left, right = plain_text.split(separator, 1)
+				# Highlight the left part (including the separator):
+				text.set_length(len(left) + len(separator))
+				self.moulti_re_hl.highlight(text)
+				# Highlight the right part:
+				right_text = Text(right)
+				self.repr_hl.highlight(right_text)
+				text.append(right_text)
+				# All done:
+				return
+		# No separator? Highlight the entire string using MoultiRegexHighlighter:
+		self.moulti_re_hl.highlight(text)

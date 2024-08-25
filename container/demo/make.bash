7ab759624b5d9b3fc75048c35789322a3c40007e
Xavier G. <xavier.moulti@kindwolf.org>
2024-08-25 22:47:22 +0200

    OCI: improve build script.
    This change ensures the moulti-demo image is built upon a base system that has
    at least one upgradable package.

--- container/demo/make.bash
+++ container/demo/make.bash
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 debian='docker.io/library/debian'
 image="xavierong/moulti-demo"
-# The latest stable image may not need any package to upgrade, so use the previous one:
-penultimate_stable=$(podman search --list-tags "${debian}" --limit=20000 --no-trunc --format='{{.Tag}}' | grep -P '^stable-\d{8}-slim$' | tail -2 | head -1)
-exec podman build --tag "${image}:latest" --tag "${image}:$(date '+%Y%m%d')" --squash --from="${debian}:${penultimate_stable}" --file=container/demo/Dockerfile ../../
+
+echo "Looking for the latest ${debian}:stable-YYYYMMDD-slim image that has upgradable packages:"
+upgradable_command='apt update &> /dev/null && apt list --upgradable 2> /dev/null | grep --quiet /'
+while read -r tag; do
+	echo -n "${debian}:${tag}..."
+	podman run --rm "${debian}:${tag}" /usr/bin/bash -c "${upgradable_command}" && break
+	echo -e "\b\b\b: no upgradable packages"
+done < <(podman search --list-tags "${debian}" --limit=20000 --no-trunc --format='{{.Tag}}' | grep -P '^stable-\d{8}-slim$' | tac)
+echo -e "\b\b\b: ok"
+
+exec podman build --tag "${image}:latest" --tag "${image}:$(date '+%Y%m%d')" --squash --from="${debian}:${tag}" --file=container/demo/Dockerfile ../../

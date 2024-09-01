39a57ffa2d23de8256fef5d37a1cac05cc86a47c
Xavier G. <xavier.moulti@kindwolf.org>
2024-09-01 16:11:08 +0200

    CollapsibleStep: introduce search() and subsearch().
    search() implements text search for common labels (title, top and bottom
    texts). Subclasses that implement subcompose() should implement subsearch() to
    offer text search.

--- src/moulti/widgets/collapsiblestep/tui.py
+++ src/moulti/widgets/collapsiblestep/tui.py
@@ -2,8 +2,16 @@ from typing import Any
 from typing_extensions import Self
 from textual.app import ComposeResult
 from textual.events import Click
+from textual.widget import Widget
 from textual.widgets import Static, Collapsible
+from moulti.search import TextSearch
 from ..abstractstep.tui import AbstractStep
+
+SEARCH_RESET = 0
+SEARCH_TITLE = 1
+SEARCH_TOP_TEXT = 2
+SEARCH_SUBWIDGETS = 3
+SEARCH_BOTTOM_TEXT = 4
 
 class CollapsibleStep(AbstractStep):
 	"""
@@ -15,6 +23,7 @@ class CollapsibleStep(AbstractStep):
 		self.collapsible = Collapsible(title=id)
 		self.top_label = Static('', classes='top_text')
 		self.bottom_label = Static('', classes='bottom_text')
+		self.search_cursor = SEARCH_RESET
 
 		self.top_text = ''
 		self.bottom_text = ''
@@ -29,6 +38,55 @@ class CollapsibleStep(AbstractStep):
 			yield self.top_label
 			yield from self.subcompose()
 			yield self.bottom_label
+
+	def subsearch(self, _: TextSearch) -> bool:
+		return False
+
+	def search_part(self, search: TextSearch) -> tuple[bool, Widget|None]:
+		if self.search_cursor == SEARCH_TITLE:
+			found, text = self.search_label(self.title, search)
+			self.collapsible.title = text.markup
+			widget = self.query_one('CollapsibleTitle') if found else None
+			return found, widget
+		if self.search_cursor == SEARCH_TOP_TEXT:
+			found, text = self.search_label(self.top_text, search)
+			self.top_label.update(text)
+			widget = self.top_label if found else None
+			return found, widget
+		if self.search_cursor == SEARCH_BOTTOM_TEXT:
+			found, text = self.search_label(self.bottom_text, search)
+			self.bottom_label.update(text)
+			widget = self.bottom_label if found else None
+			return found, widget
+		if self.search_cursor == SEARCH_SUBWIDGETS:
+			return self.subsearch(search), None
+		return False, None
+
+	def search(self, search: TextSearch) -> bool:
+		found = False
+		widget = None
+		if search.next_result:
+			parts = (SEARCH_TITLE, SEARCH_TOP_TEXT, SEARCH_SUBWIDGETS, SEARCH_BOTTOM_TEXT, SEARCH_RESET)
+		else:
+			parts = (SEARCH_BOTTOM_TEXT, SEARCH_SUBWIDGETS, SEARCH_TOP_TEXT, SEARCH_TITLE, SEARCH_RESET)
+		if self.search_cursor == SEARCH_RESET:
+			self.search_cursor = parts[0]
+		while self.search_cursor != SEARCH_RESET:
+			found, widget = self.search_part(search)
+			if found:
+				break
+			self.search_cursor = parts[parts.index(self.search_cursor) + 1]
+		if found:
+			# Expand this collapsible step if necessary:
+			if self.search_cursor != SEARCH_TITLE:
+				self.collapsible.collapsed = False
+			# Grab the focus, except for subwidgets, which have to grab focus explicitly:
+			if self.search_cursor != SEARCH_SUBWIDGETS:
+				self.focus(False)
+			# Scroll to make the highlighted result visible:
+			if widget is not None:
+				self.post_message(self.ScrollRequest(self, widget, center=True, animate=False))
+		return found
 
 	def on_mount(self) -> None:
 		self.query_one('CollapsibleTitle').tooltip = f'Step id: {self.title_from_id()}'

8dbf5a371c7a5bd0396da1538e1e467bfca0a1ae
Xavier G. <xavier.moulti@kindwolf.org>
2024-09-14 19:32:39 +0200

    Add extra metadata.

--- src/moulti/__init__.py
+++ src/moulti/__init__.py
@@ -1,2 +1,6 @@
-__author__ = 'Xavier G. <xavier.moulti@kindwolf.org>'
+__author__ = 'Xavier G.'
+__copyright__ = 'Copyright (c) 2024 Xavier G.'
+__email__ = 'xavier.moulti@kindwolf.org'
+__licence__ = 'MIT'
+__maintainer__ = __author__
 __version__ = '1.18.1'

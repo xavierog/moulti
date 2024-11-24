b0f9a9e04a3da9c3b39bda39706cc0a0a12e6d9d
Xavier G. <xavier.moulti@kindwolf.org>
2024-11-24 20:36:31 +0100

    Tests: shell scripts: improve portability.

--- tests/scripts/diff.bash
+++ tests/scripts/diff.bash
@@ -2,5 +2,5 @@
 moulti set --title='git show ba89966c1984'
 export MOULTI_DIFF_NO_TITLE=yes
 export MOULTI_DIFF_VERBOSE=yes
-first_step=$(moulti diff parse tests/data/linux-kernel-ba89966c1984.patch | head -1 | sed 's/\w* //')
+first_step=$(moulti diff parse tests/data/linux-kernel-ba89966c1984.patch | head -1 | sed 's/[a-z]* //')
 moulti step update "${first_step}" --max-height=7

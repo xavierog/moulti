eb25340b82a7592e7d01a6610c08cc34ee2a6ba0
Xavier G. <xavier.moulti@kindwolf.org>
2024-11-27 01:11:15 +0100

    Tests: test_scrolling: reduce the odds of a race condition.

--- tests/test_scrolling.py
+++ tests/test_scrolling.py
@@ -1,4 +1,4 @@
-from .common import moulti_test, moulti_test_steps, wait_script, wait_script_start, wait_script_end
+from .common import Beacon, moulti_test, moulti_test_steps, wait_script, wait_script_start, wait_script_end
 assert moulti_test
 
 def test_programmatic_scrolling(moulti_test):
@@ -16,10 +16,19 @@ def test_lock_scroll(moulti_test):
 		await wait_script(pilot)
 	assert moulti_test(command=['manual-scrolling-through-steps.bash'], run_before=scroll_lock)
 
-def test_auto_scrolling(moulti_test):
+def test_auto_scrolling(moulti_test, monkeypatch):
+	beacon = Beacon(monkeypatch)
 	async def interrupt_auto_scroll(pilot):
 		await wait_script_start(pilot)
+		await beacon.wait(pilot, '-output-started')
 		await pilot.pause(0.5)
 		await pilot.press('tab', 'tab', 'home') # focus the first step, focus its log, hit the 'home' key
 		await wait_script_end(pilot)
+		# Wait until the first step has finished scrolling back up to the top:
+		log_widget = pilot.app.steps_container.children[0].log_widget
+		interval = 0.1
+		max_wait = int(45 / interval)
+		while max_wait and log_widget.scroll_y:
+			await pilot.pause(interval)
+			max_wait -= 1
 	assert moulti_test(command=['scrolling-inside-steps.bash'], run_before=interrupt_auto_scroll)

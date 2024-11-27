8e52f6268c2d9b6bfc16e3a18af1bf2df9fede92
Xavier G. <xavier.moulti@kindwolf.org>
2024-11-27 01:11:15 +0100

    Tests: test_scrolling: reduce the odds of a race condition.

--- tests/scripts/scrolling-inside-steps.bash
+++ tests/scripts/scrolling-inside-steps.bash
@@ -11,7 +11,9 @@ function output {
 moulti step add 1 --min-height=10 --max-height=10 --title='Step with auto-scroll enabled by default'
 moulti step add 2 --min-height=10 --max-height=10 --title='Step with auto-scroll disabled' --no-auto-scroll
 moulti step add 3 --min-height=10 --max-height=10 --title='Step with auto-scroll enabled by default'
+
 output 40 'interrupted auto-scrolling' | moulti pass 1 &
 output 40 'no auto-scrolling'          | moulti pass 2 &
 output 40 'auto-scrolling'             | moulti pass 3 &
+touch "${MOULTI_TEST_BEACON}-output-started"
 wait

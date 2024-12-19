d1c86e3136dfb44474645e6d0a4dfaf43560f383
Xavier G. <xavier.moulti@kindwolf.org>
2024-12-19 22:30:39 +0100

    Tag v1.24.0.

--- src/moulti/__init__.py
+++ src/moulti/__init__.py
@@ -3,4 +3,4 @@ __copyright__ = 'Copyright (c) 2024 Xavier G.'
 __email__ = 'xavier.moulti@kindwolf.org'
 __licence__ = 'MIT'
 __maintainer__ = __author__
-__version__ = '1.23.2'
+__version__ = '1.24.0'

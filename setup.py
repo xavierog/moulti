69c49b02da81db6219757d9adcc84904f2623973
Xavier G. <xavier.moulti@kindwolf.org>
2024-12-27 21:26:21 +0100

    Tag v1.26.0.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.md', 'r') as readme:
 
 setup(
 	name='moulti',
-	version='1.25.0',
+	version='1.26.0',
 	description=short_description,
 	long_description=long_description,
 	long_description_content_type='text/markdown',

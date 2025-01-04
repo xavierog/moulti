c760f7ba4c289e29fc8f573bdc71c771e3c9010c
Xavier G. <xavier.moulti@kindwolf.org>
2025-01-04 18:22:53 +0100

    Bump copyright.

--- src/moulti/__init__.py
+++ src/moulti/__init__.py
@@ -1,5 +1,5 @@
 __author__ = 'Xavier G.'
-__copyright__ = 'Copyright (c) 2024 Xavier G.'
+__copyright__ = 'Copyright (c) 2024-2025 Xavier G.'
 __email__ = 'xavier.moulti@kindwolf.org'
 __licence__ = 'MIT'
 __maintainer__ = __author__
